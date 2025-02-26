1ab523891c0a20dc721ea84c274c9ded644527bd
akashwarkhad151 <akashwarkhad151@gmail.com>
2025-02-26 09:02:14 +0530

    Add Models & DbContxt

---
+++ EntityFrameworkCore/EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data
+{
+    internal class FootballLeagueDbContext : DbContext
+    {
+        public FootballLeagueDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Coach> Coaches { get; set; }
+
+        // Configurtion for SqlServer 
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootbalLeague_EfCore; Encrypt= False");
+        //}
+
+        // Configuration for Sqlite
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source=FootbalLeague_EfCore.db");
+        }
+    }
+}
---
+++ EntityFrameworkCore/EntityFrameworkCore.Domain/BaseDomainModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+    // This is base class & cannot be instantiated Its just able to extend
+    public abstract class BaseDomainModel
+    {
+        public DateTime CreatedAt { get; set; }
+    }
+}
