7172c07487be4c0791145a3b85781705a807d1c2
akashwarkhad151 <akashwarkhad151@gmail.com>
2025-02-27 12:30:11 +0530

    Added New DBContext + Sedding + Migrations

--- EntityFrameworkCore/EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore/EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -4,26 +4,48 @@ using Microsoft.EntityFrameworkCore;
 namespace EntityFrameworkCore.Data
 {
     internal class FootballLeagueDbContext : DbContext
-    {
-        public FootballLeagueDbContext(DbContextOptions options) : base(options)
-        {
-        }
-
+    {     
         public DbSet<Team> Teams { get; set; }
 
         public DbSet<Coach> Coaches { get; set; }
 
-        // Configurtion for SqlServer 
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootbalLeague_EfCore; Encrypt= False");
-        //}
-
-        // Configuration for Sqlite
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite($"Data Source=FootbalLeague_EfCore.db");
+            // Configurtion for SqlServer 
+            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore; Encrypt= False");
+
+            // Configurtion for Sql Lite
+            optionsBuilder.UseSqlite($"Data Source=FootballLeague_EfCore.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().HasData(
+                new Team
+                {
+                    TeamId = 1,
+                    Name = "CSK Team",
+                    CreatedAt = new DateTime(2025, 02, 12)
+                },
+                new Team
+                {
+                    TeamId = 2,
+                    Name = "MI Team",
+                    CreatedAt = new DateTime(2025, 02, 13)
+                },
+                new Team
+                {
+                    TeamId = 3,
+                    Name = "KKR Team",
+                    CreatedAt = new DateTime(2025, 02, 14)
+                });
+
+            modelBuilder.Entity<Coach>().HasData(new Coach 
+            {
+                Id = 4,
+                Name = "MK Sir",
+                CreatedAt = new DateTime(2025, 02, 13)
+            });
         }
     }
 }
