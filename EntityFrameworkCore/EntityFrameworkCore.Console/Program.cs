94772b4093dd1e5344032f63eb92ff32a3904f3d
akashwarkhad151 <akashwarkhad151@gmail.com>
2025-03-07 12:51:06 +0530

    Add LinQ Operations

--- EntityFrameworkCore/EntityFrameworkCore.Console/Program.cs
+++ EntityFrameworkCore/EntityFrameworkCore.Console/Program.cs
@@ -1,23 +1,63 @@
 ﻿using EntityFrameworkCore.Data;
 using Microsoft.EntityFrameworkCore;
 
-try
+using var context = new FootballLeagueDbContext();
+
+// Fetch All Teams data from the DB
+await GetAllTeams();
+
+// Fetch Teams data from the DB By Id
+await GetTeamById();
+
+// Get data By Filter
+await GetTeamByFilter();
+
+
+async Task GetTeamByFilter()
 {
-    var context = new FootballLeagueDbContext();
+    Console.Write("---------------------------------- Enter a Team name :");
+    var readData = Console.ReadLine();
 
-    // Fetch the Teams data from the DB
-    //  SELECT * FROM Teams;
-
-    Console.WriteLine($"Database Path: {context.Database.GetDbConnection().ConnectionString}");
-
-    var teams = context.Teams.ToList();
-
-    foreach (var team in teams)
+    var data = await context.Teams.Where(x => x.Name == readData).ToListAsync();
+    foreach (var team in data)
     {
-        Console.WriteLine(team.Name);
+        Console.WriteLine($"{team.Name}, {team.CreatedAt}, {team.TeamId}");
     }
+    Console.Write("---------------------------------- Data ^:");
 }
-catch(Exception ex)
+
+async Task GetTeamById()
 {
-    Console.WriteLine(ex.Data.ToString() + Environment.NewLine + ex.Message);
+    // Using First Here Below 1, 2 will gives exception when no data found.
+    var t1 = context.Teams.First(x => x.TeamId == 1);
+    var t2 = await context.Teams.FirstAsync(x => x.TeamId == 1);
+    var t3 = context.Teams.FirstOrDefault(x => x.TeamId == 1);
+    var t4 = await context.Teams.FirstOrDefaultAsync(x=> x.TeamId == 1);
+
+    // Using Single Here Below 5, 6 will gives exception when no data found.
+    var t5 = context.Teams.Single(x => x.TeamId == 1);
+    var t6 = await context.Teams.SingleAsync(x => x.TeamId == 1);
+    var t7 = context.Teams.SingleOrDefault(x => x.TeamId == 1);
+    var t8 = await context.Teams.SingleOrDefaultAsync(x => x.TeamId == 1);
+
+    // Using Find No one gives you exception when n
+    var t9 = context.Teams.Find(100);
+    var t10 = await context.Teams.FindAsync(1);
+}
+
+async Task GetAllTeams()
+{
+    try
+    {
+        var teams = await context.Teams.ToListAsync();
+
+        foreach (var team in teams)
+        {
+            Console.WriteLine(team.Name);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Data.ToString() + Environment.NewLine + ex.Message);
+    }
 }
